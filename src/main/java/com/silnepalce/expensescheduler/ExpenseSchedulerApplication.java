b7500b4edbfcb7d848edc2ce08e735e28945d057
krystian-tokarz <krystiantokarz@wp.pl>
2022-01-25 20:20:41 +0100

    First commit

---
+++ src/main/java/com/silnepalce/expensescheduler/ExpenseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.silnepalce.expensescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseSchedulerApplication.class, args);
+    }
+
+}
